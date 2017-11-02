ef503887140850dcab2da4e90a26f5dabc28ce69
Odaryna <odaryna.korolevych@gmail.com>
2017-11-02 14:43:25 +0200

    Initial Commit

---
+++ Search of nulls/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Search of nulls
+//
+//  Created by Odaryna on 11/2/17.
+//  Copyright © 2017 Odaryna. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Search of nulls/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Search of nulls
+//
+//  Created by Odaryna on 11/2/17.
+//  Copyright © 2017 Odaryna. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
